70fd3d9a3fbb809fef8fab345aaacdf3b1464e2d
zy51927042 <zy51927042@163.com>
2022-07-22 20:51:04 +0800

    Configure feign client for http basic auth, closes #13

--- src/main/java/guru/springframework/msscbeerservice/services/inventory/InventoryServiceFeignClient.java
+++ src/main/java/guru/springframework/msscbeerservice/services/inventory/InventoryServiceFeignClient.java
@@ -1,5 +1,6 @@
 package guru.springframework.msscbeerservice.services.inventory;
 
+import guru.springframework.msscbeerservice.config.FeignClientConfig;
 import guru.springframework.msscbeerservice.services.inventory.model.BeerInventoryDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
 import java.util.UUID;
 
-@FeignClient(name = "inventory-service", fallback = InventoryServiceFeignClientFailover.class)
+@FeignClient(name = "inventory-service", fallback = InventoryServiceFeignClientFailover.class, configuration = FeignClientConfig.class)
 public interface InventoryServiceFeignClient {
     @RequestMapping(method = RequestMethod.GET, value = BeerInventoryServiceRestTemplateImpl.INVENTORY_PATH)
     ResponseEntity<List<BeerInventoryDto>> getOnhandInventory(@PathVariable UUID beerId);
